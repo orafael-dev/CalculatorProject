0a6ea9ec84c12bc7833c6c17279b7299e98bdcd0
Rafael Almeida <almeidarp.dev@gmail.com>
2024-07-16 18:37:21 -0300

    feat: Adição e correção de webpack, babel e loaders

--- src/index.js
+++ src/index.js
@@ -1,16 +1,21 @@
-import calculate from "./calculate.js"
-import copyToClipboard from "./copyToClipboard.js"
-import { handleButtonPress, handleClear, handleTyping } from "./keyHandlers.js"
-import themeSwitcher from "./themeSwitcher.js"
+import "./styles/index.css";
+import calculate from "./calculate.js";
+import copyToClipboard from "./copyToClipboard.js";
+import { handleButtonPress, handleClear, handleTyping } from "./keyHandlers.js";
+import themeSwitcher from "./themeSwitcher.js";
 
-const input = document.getElementById('input')
-const resultInput = document.getElementById('result')
+const input = document.getElementById("input");
+const resultInput = document.getElementById("result");
 
-document.querySelectorAll('.charKey').forEach(function (charKeyBtn) {
-    charKeyBtn.addEventListener('click', handleButtonPress)
-})
-document.getElementById('clear').addEventListener('click', handleClear)
-document.getElementById('input').addEventListener('keydown', handleTyping )
-document.getElementById('operatorEqual').addEventListener('click', calculate)
-document.getElementById('copyToClipboard').addEventListener('click', copyToClipboard)
-document.getElementById('themeSwitcher').addEventListener('click', themeSwitcher)
+document.querySelectorAll(".charKey").forEach(function (charKeyBtn) {
+  charKeyBtn.addEventListener("click", handleButtonPress);
+});
+document.getElementById("clear").addEventListener("click", handleClear);
+document.getElementById("input").addEventListener("keydown", handleTyping);
+document.getElementById("operatorEqual").addEventListener("click", calculate);
+document
+  .getElementById("copyToClipboard")
+  .addEventListener("click", copyToClipboard);
+document
+  .getElementById("themeSwitcher")
+  .addEventListener("click", themeSwitcher);
--- webpack.config.js
+++ webpack.config.js
@@ -1,6 +1,21 @@
 module.exports = {
   entry: {
-    main: "./src/index.js",
+    index: "./src/index.js",
   },
   mode: "production",
+  module: {
+    rules: [
+      {
+        test: /\.css$/,
+        use: ["style-loader", "css-loader"],
+      },
+      {
+        test: /.js$/,
+        use: ["babel-loader"],
+      },
+    ],
+  },
+  output: {
+    filename: "[name].min.js",
+  },
 };
